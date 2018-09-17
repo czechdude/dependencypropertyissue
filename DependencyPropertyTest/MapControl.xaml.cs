d04ce3cde83259ff91608ca1a57d0bbe94b56945
Petr Divis <petr.divis@geocom.ch>
2018-09-17 13:27:39 +0200

    reproduce issue with dependency property problem

---
+++ DependencyPropertyTest/MapControl.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+
+namespace DependencyPropertyTest
+{
+    /// <summary>
+    /// Interaction logic for MapControl.xaml
+    /// </summary>
+    public partial class MapControl : UserControl
+    {
+        public MapControl()
+        {
+            InitializeComponent();
+            SizeChanged += OnControlSizeChanged;
+
+            MapView.ViewpointChanged += OnViewPointChanged;
+            MapView.SizeChanged += OnMapViewSizeChanged;
+            MapView.InteractionOptions.IsRotateEnabled = true;
+        }
+
+        private void OnMapViewSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            MapWidth = MapView.ActualWidth;
+            MapHeight = MapView.ActualHeight;
+        }
+
+        public static readonly DependencyProperty ActualControlWidthProperty = DependencyProperty.Register(
+            "ActualControlWidth",
+            typeof(double),
+            typeof(MapControl),
+            new FrameworkPropertyMetadata(PropertyChangedCallback));
+
+        private static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var newVar = e.NewValue;
+        }
+
+        public double ActualControlWidth
+        {
+            get { return (double)GetValue(ActualControlWidthProperty); }
+            set { SetValue(ActualControlWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty ActualControlHeightProperty = DependencyProperty.Register(
+            "ActualControlHeight",
+            typeof(double),
+            typeof(MapControl),
+            new FrameworkPropertyMetadata(default(double)));
+
+        public double ActualControlHeight
+        {
+            get { return (double) GetValue(ActualControlHeightProperty); }
+            set { SetValue(ActualControlHeightProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty ViewPointProperty = DependencyProperty.Register(
+            "ViewPoint",
+            typeof(Geometry),
+            typeof(MapControl),
+            new FrameworkPropertyMetadata(OnViewPointChanged));
+
+        private static void OnViewPointChanged(DependencyObject d, DependencyPropertyChangedEventArgs a)
+        {
+            if (d != null && a.NewValue != null)
+                ((MapControl)d).SetViewPoint((Geometry)a.NewValue);
+        }
+
+        private void OnViewPointChanged(object sender, EventArgs e)
+        {
+            MapExtent = MapView.GetCurrentViewpoint(ViewpointType.BoundingGeometry)?.TargetGeometry?.Extent;
+        }
+
+        public static readonly DependencyProperty MapWidthProperty = DependencyProperty.Register(
+            "MapWidth",
+            typeof(double),
+            typeof(MapControl),
+            new PropertyMetadata(default(double)));
+
+        public double MapWidth
+        {
+            get { return (double)GetValue(MapWidthProperty); }
+            set { SetValue(MapWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty MapHeightProperty = DependencyProperty.Register(
+            "MapHeight",
+            typeof(double),
+            typeof(MapControl),
+            new PropertyMetadata(default(double)));
+
+        public double MapHeight
+        {
+            get { return (double)GetValue(MapHeightProperty); }
+            set { SetValue(MapHeightProperty, value); }
+        }
+
+        public static readonly DependencyProperty MapExtentProperty = DependencyProperty.Register(
+            "MapExtent",
+            typeof(Envelope),
+            typeof(MapControl),
+            new PropertyMetadata(default(Envelope)));
+
+        public Envelope MapExtent
+        {
+            get { return (Envelope)GetValue(MapExtentProperty); }
+            set { SetValue(MapExtentProperty, value); }
+        }
+
+        private void OnControlSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ActualControlWidth = ActualWidth;
+            ActualControlHeight = ActualHeight;
+        }
+
+        public Geometry ViewPoint
+        {
+            get { return (Geometry)GetValue(ViewPointProperty); }
+            set
+            {
+                SetValue(ViewPointProperty, value);
+                SetViewPoint(value);
+            }
+        }
+
+        private void SetViewPoint(Geometry viewPoint)
+        {
+            MapView.SetViewpointGeometryAsync(viewPoint);
+        }
+    }
+}
---
+++ DependencyPropertyTest/ViewModel/MapViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Esri.ArcGISRuntime;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Portal;
+using Esri.ArcGISRuntime.Security;
+using GalaSoft.MvvmLight;
+
+namespace DependencyPropertyTest
+{
+    public class MapViewModel : ViewModelBase
+    {
+        private double _actualControlWidth;
+        private double _actualControlHeight;
+        private Map _map;
+        private Envelope _mapViewGeometry;
+        private Envelope _mapExtent;
+        private double _actualMapWidth;
+        private double _actualMapHeight;
+
+        public MapViewModel()
+        {
+            ArcGISRuntimeEnvironment.Initialize();
+            var uri = new Uri("http://petr-divis.maps.arcgis.com/sharing/rest");
+
+            var portal = ArcGISPortal.CreateAsync(uri).Result;
+
+            // once connected, get the license info from the portal for the current user
+            var licenseInfo = portal.PortalInfo.LicenseInfo;
+
+            // set the license using the static ArcGISRuntimeEnvironment class
+            ArcGISRuntimeEnvironment.SetLicense(licenseInfo);
+
+            var portalItem = PortalItem.CreateAsync(portal, "69fdcd8e40734712aaec34194d4b988c").Result;
+
+            Map = new Map(portalItem);
+        }
+
+        public Map Map
+        {
+            get => _map;
+            set => Set(() => Map, ref _map, value);
+        }
+
+        public double ActualControlWidth
+        {
+            get => _actualControlWidth;
+            set => _actualControlWidth = value;
+        }
+
+        public double ActualControlHeight
+        {
+            get => _actualControlHeight;
+            set => _actualControlHeight = value;
+        }
+
+        public double ActualMapWidth
+        {
+            get => _actualMapWidth;
+            set => _actualMapWidth = value;
+        }
+
+        public double ActualMapHeight
+        {
+            get => _actualMapHeight;
+            set => _actualMapHeight = value;
+        }
+
+        public Envelope MapViewGeometry
+        {
+            get => _mapViewGeometry;
+            set => Set(() => MapViewGeometry, ref _mapViewGeometry, value);
+        }
+
+        public Envelope MapExtent
+        {
+            get => _mapExtent;
+            set
+            {
+                Set(() => MapExtent, ref _mapExtent, value);
+               
+            }
+        }
+    }
+}
